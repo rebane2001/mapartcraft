0eb3bd70a05cf9ff9f61de410acc6d8bcf00f869
rebane2001 <rebane2001@gmail.com>
2020-02-29 15:20:01 +0200

    Added map.dat mappings

--- js/colorlist.js
+++ js/colorlist.js
@@ -687,3 +687,4 @@ window.colorlist_patches = {
     [[50,0], ['black_terracotta', '', 'Black Terracotta', false, 'hardened_clay_stained_black', 0]],
   ]
 }
+window.mapdatmappings = [1,2,3,4,5,6,7,9,10,11,12,13,14,8,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];
