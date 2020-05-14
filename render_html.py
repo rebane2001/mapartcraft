4d6dd6e2fd8f7cb2edff8cd50ae3464498f18bea
rebane2001 <rebane2001@gmail.com>
2020-05-14 20:52:37 +0300

    Added support for unicode (eg Russian) translations

--- render_html.py
+++ render_html.py
@@ -8,20 +8,20 @@ root_path = "/web"
 for langjsonfile in os.listdir('languages'):
     langcode = langjsonfile.split(".")[0]
     print(f"Creating HTML for {langcode}")
-    with open(f"languages/{langjsonfile}","r") as f:
+    with open(f"languages/{langjsonfile}","r", encoding="UTF8") as f:
         langjson = json.load(f)
     # Hack to edit the css files
     langjson["css/spritesheet.css"] = {}
     langjson["css/app.css"] = {}
     shutil.copytree("template", f"web/{langcode}", dirs_exist_ok=True)
     for path in langjson:
-        with open(f"web/{langcode}/{path}", "r") as f:
+        with open(f"web/{langcode}/{path}", "r", encoding="UTF8") as f:
             target = f.read()
         target = target.replace(f"%%ROOTPATH%%",root_path)
         target = target.replace(f"%%HTMLLANG%%",langcode)
         for key, value in langjson[path].items():
             target = target.replace(f"%%{key}%%",value)
-        with open(f"web/{langcode}/{path}", "w") as f:
+        with open(f"web/{langcode}/{path}", "w", encoding="UTF8") as f:
             f.write(target)
 
 for f in os.listdir("web/en/"):
