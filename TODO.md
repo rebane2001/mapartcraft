828954a44f0f7b7e4d4139f0d4bcabe00ed6f13c
SelfAdjointOperator <66702639+SelfAdjointOperator@users.noreply.github.com>
2021-06-18 02:38:56 +0100

    add tools/addMinecraftVersion.py (1.17 ready), update todo, faq styles

--- TODO.md
+++ TODO.md
@@ -4,7 +4,6 @@
 
 # Yes
 
-- Warning for non-forwards compatible blocks eg iron bars, 1.13 brown kelp
 - Move coloursJSON to JSONs folder also add head
 - 1.17 Blocks
 - Look at issues on Rebane repo
@@ -14,10 +13,12 @@
 - CLI
 
 # Maybe
-- Flammable parameter glass layer
-- 2.5D maps (jkascpkmc's Fox for inspo)
 - Linear / none interpolation option for small uploads if easy
 - Hall of fame
+
+# Probably Not
+- 2.5D maps (jkascpkmc's Fox for inspo)
+- Flammable parameter glass layer
 
 # Code Refactoring
 
--- src/components/faq.js
+++ src/components/faq.js
@@ -6,29 +6,29 @@ function FAQ(props) {
   return (
     <div className="FAQ">
       <Link to={`/${![undefined, "en"].includes(props.match.params.countryCode) ? props.match.params.countryCode : ""}`}>
-        <h3 className="closeTextButton">Close</h3>
+        <h3>Close</h3>
       </Link>
 
       <h1>FAQ</h1>
 
-      <h2 className="h2_FAQ">How do I get started?</h2>
+      <h2>How do I get started?</h2>
       <a href="https://youtu.be/bJ-wX68WNHM" target="_blank" rel="noopener noreferrer">
         Video tutorial
       </a>
 
-      <h2 className="h2_FAQ">General</h2>
+      <h2>General</h2>
       <b>Why does the output change with the same image and settings?</b>
-      <p className="p_FAQ">
+      <p>
         The JPEG decoding and scaling algorithms vary between browsers and get changed all the time. If you wish to be 100% sure your image stays the same,
-        right click on the <i>Map preview</i> and choose <i>Save image as...</i>. Next time you can upload the image you saved.
+        right click on the <em>Map preview</em> and choose <em>Save image as...</em>. Next time you can upload the image you saved.
       </p>
 
-      <h2 className="h2_FAQ">Schematic</h2>
+      <h2>Schematic (.nbt)</h2>
       <b>What do I do with the NBT file?</b>
-      <p className="p_FAQ">Use it like a .schematic file.</p>
+      <p>Use it like a .schematic file.</p>
 
       <b>How can I use the NBT file?</b>
-      <p className="p_FAQ">
+      <p>
         You can use it with programs like{" "}
         <a href="https://cubical.xyz/" target="_blank" rel="noopener noreferrer">
           cubical.xyz
@@ -53,7 +53,7 @@ function FAQ(props) {
       </p>
 
       <b>...but I need a .schematic file!</b>
-      <p className="p_FAQ">
+      <p>
         Import it into{" "}
         <a href="https://cubical.xyz/" target="_blank" rel="noopener noreferrer">
           cubical.xyz
@@ -62,12 +62,12 @@ function FAQ(props) {
         <a href="https://www.mcedit-unified.net/" target="_blank" rel="noopener noreferrer">
           MCEdit
         </a>{" "}
-        and export as .schematic. <i>Note that if you're using MCEdit or Cubical, you must export the map as 1.12.2.</i> You do not need a .schematic file for
+        and export as .schematic. <em>Note that if you're using MCEdit or Cubical, you must export the map as 1.12.2.</em> You do not need a .schematic file for
         use with Baritone; use Schematica and the "#schematica" command.
       </p>
 
-      <b>But MCEdit doesn't work</b>
-      <p className="p_FAQ">
+      <b>But MCEdit doesn't work!</b>
+      <p>
         Make sure you're using{" "}
         <a href="https://www.mcedit-unified.net/" target="_blank" rel="noopener noreferrer">
           MCEdit Unified
@@ -76,67 +76,67 @@ function FAQ(props) {
       </p>
 
       <b>Why is there a row of extra blocks?</b>
-      <p className="p_FAQ">
+      <p>
         The shade a block shows up as on the map is decided by the block North of it; if the Northern block is higher then a darker tone shows, else if the
         Northern block is lower then a lighter tone shows, else a normal tone shows. Thus an extra row of blocks (colloquially known as a noobline) exists at
         the top of a map to shade the top row properly.
       </p>
 
       <b>How do I align the map?</b>
-      <p className="p_FAQ">
+      <p>
         When you have found a suitable place to build your map (eg above an ocean) make sure to open the map first and find the bottom left corner for aligning
         your schematic. Maps in Minecraft align to a fixed 128x128 grid. North is always the up-direction on maps and you shouldn't rotate the schematic.
       </p>
 
-      <h2 className="h2_FAQ">Datafile</h2>
+      <h2>Datafile (.dat)</h2>
       <b>What is a map.dat file?</b>
-      <p className="p_FAQ">
+      <p>
         '.dat' is the native format Minecraft stores map data in, meaning you can use it to import maps into your worlds without needing to build a physical
         structure. It also enables you to use a fourth extra shade of color not accessible in survival.
       </p>
 
       <b>What do I do with the map.dat file?</b>
-      <p className="p_FAQ">
+      <p>
         You can use the map.dat file in singleplayer or a server you own. Create a new map in-game, go to your world's save file, then the data folder and from
         there you can replace map_xxx.dat files.
       </p>
 
-      <h2 className="h2_FAQ">Settings</h2>
+      <h2>Settings</h2>
       <b>Map size?</b>
-      <p className="p_FAQ">
+      <p>
         This will define how many maps you will create for your picture. When creating bigger maps, it's recommended to split it into multiple schematics (lest
         large staircased maps stretch above the world height limit). This can be done with the "DOWNLOAD AS 1X1 SPLIT" button.
       </p>
 
       <b>My image is stretched!</b>
-      <p className="p_FAQ">Change your map size, enable the crop option, or edit your image with an image editor.</p>
+      <p>Change your map size, enable the crop option, or edit your image with an image editor.</p>
 
       <b>'Staircasing'?</b>
-      <p className="p_FAQ">
+      <p>
         This will make your map 3D. Doing so will give you 3 times the colors, often producing a much richer mapart, but it will also make the map a lot harder
         to build, as it is not flat.
       </p>
 
       <b>Better color?</b>
-      <p className="p_FAQ">
+      <p>
         This setting will give you more natural colors. Disabling this will make the website faster and give you slightly worse colors. It is recommended to
         keep this enabled.
       </p>
 
       <b>Dithering?</b>
-      <p className="p_FAQ">
+      <p>
         This will add grain to your image to make it look a lot smoother. Floyd-Steinberg dithering is the most accurate, but the Ordered/Bayer dithering will
         usually have less artifacts and gives the image an unique style. It is usually recommended to disable dithering for flat-colored artwork.
       </p>
 
       <b>Add blocks under/Block to add?</b>
-      <p className="p_FAQ">
+      <p>
         Here you can pick the block that will be put under either important blocks (eg carpets, sand, pressure plates) or all blocks. This block will also be
         used for the noobline, which cannot be disabled.
       </p>
 
       <b>Presets?</b>
-      <p className="p_FAQ">
+      <p>
         You can use presets to save and load block configurations. Pick your blocks and click "Save" to save them as a preset, pick a preset to load it and
         click "Delete" to delete the loaded preset. It is also possible to share a link for your preset with others.
       </p>
